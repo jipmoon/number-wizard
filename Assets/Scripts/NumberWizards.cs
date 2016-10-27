dc35aa1d8f97e3898dcb51158a85df43f0a287d2
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-10-27 00:13:48 -0700

    Complete Number Wizard Console

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -3,19 +3,25 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour {
 	// Use this for initialization
-	int max = 1000;
-	int min = 1;
-	int guess = 500;
+	int max;
+	int min;
+	int guess;
 	
 	void Start () {
-		StartGame ();
+		StartGame();
 	}
 	
 	void StartGame () {
+	
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		max = max + 1;
+		
+		print ("========================");
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
-		
-		
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
